164696c7cbe1c35ee25c2e83c7a71b67a04184eb
Reiji Terasaka <terasaka@smarprise.co.jp>
2018-06-13 18:57:22 +0900

    demo

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map'
+};
+
